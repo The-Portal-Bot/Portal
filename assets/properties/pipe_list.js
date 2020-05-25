e9c069bf0f5d976ebafc2b73632676cbecb5b826
keybraker <iantsiakkas@gmail.com>
2020-05-26 01:16:25 +0300

    fix pipe implementation so that you can write multiple pipes

--- assets/properties/pipe_list.js
+++ assets/properties/pipe_list.js
@@ -1,4 +1,4 @@
-const vocal = require('voca');
+const voca = require('voca');
 
 module.exports =
 {
@@ -7,48 +7,48 @@ module.exports =
 		{
 			name: 'upperCase',
 			description: 'returns an upperCase of the input',
-			get: (str, count) => { return vocal.upperCase(str); }
+			get: (str, count) => { return voca.upperCase(str); }
 		},
 		{
 			name: 'lowerCase',
 			description: 'returns an lowerCase of the input',
-			get: (str, count) => { return vocal.lowerCase(str); }
+			get: (str, count) => { return voca.lowerCase(str); }
 		},
 		{
 			name: 'capitalize',
 			description: 'returns an capitalize of the input',
-			get: (str, count) => { return vocal.capitalize(str); }
+			get: (str, count) => { return voca.capitalize(str); }
 		},
 		{
 			name: 'decapitalize',
 			description: 'returns an decapitalize of the input',
-			get: (str, count) => { return vocal.decapitalize(str); }
+			get: (str, count) => { return voca.decapitalize(str); }
 		},
 		{
 			name: 'kebabCase',
 			description: 'returns an kebabCase of the input',
-			get: (str, count) => { return vocal.kebabCase(str); }
+			get: (str, count) => { return voca.kebabCase(str); }
 		},
 		{
 			name: 'snakeCase',
 			description: 'returns an snakeCase of the input',
-			get: (str, count) => { return vocal.snakeCase(str); }
+			get: (str, count) => { return voca.snakeCase(str); }
 		},
 		{
 			name: 'titleCase',
 			description: 'returns an titleCase of the input',
-			get: (str, count) => { return vocal.titleCase(str); }
+			get: (str, count) => { return voca.titleCase(str); }
 		},
 		{
 			name: 'camelCase',
 			description: 'returns an camelCase of the input',
-			get: (str, count) => { return vocal.camelCase(str); }
+			get: (str, count) => { return voca.camelCase(str); }
 		},
 		{
 			name: 'acronym',
 			description: 'returns an acronym of the input',
 			get: (str, count) => { 
-				return vocal.chain(str).upperCase().words().value().map(word => {
+				return voca.chain(str).upperCase().words().value().map(word => {
 					return word.charAt(0);
 				}).join('');
 			}
@@ -56,36 +56,20 @@ module.exports =
 		{
 			name: 'word',
 			description: 'returns word of the input',
-			get: (str, count) => { return vocal.words(str).slice(0, count); }
+			get: (str, count) => { return voca.words(str).slice(0, count); }
 		},
 		{
 			name: 'populous_count',
 			description: 'returns the count of most common element in list',
 			get: (str, count) => {
-				let words = str.split(' '), mf = 1, m = 0, item = words[0];
-				for (let i = 0; i < words.length; i++) {
-					for (let j = i; j < words.length; j++) {
-						if (words[i] == words[j]) { m++; }
-						if (mf < m) { mf = m; item = words[i]; }
-					}
-					m = 0;
-				}
-				return mf;
+				
 			}
 		},
 		{
 			name: 'populous',
 			description: 'returns the name of the most common element in list',
 			get: (str, count) => {
-				let words = str.split(' '), mf = 1, m = 0, item = words[0];
-				for (let i = 0; i < words.length; i++) {
-					for (let j = i; j < words.length; j++) {
-						if (words[i] == words[j]) { m++; }
-						if (mf < m) { mf = m; item = words[i]; }
-					}
-					m = 0;
-				}
-				return item;
+				
 			}
 		},
 		{
