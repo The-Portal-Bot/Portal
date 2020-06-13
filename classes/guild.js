baa944fe34b18331a13337f391e574dfcf2d58e9
keybraker <iantsiakkas@gmail.com>
2020-06-13 13:57:57 +0300

    url update to be able to set existing channel to a url channel

--- classes/guild.js
+++ classes/guild.js
@@ -1,6 +1,5 @@
 module.exports = class {
-    constructor(portal_list, url_list, role_list, spotify,
-        announcement, locale) {
+    constructor(portal_list, url_list, role_list, spotify, announcement, locale) {
         this.portal_list = portal_list;
         this.url_list = url_list;
         this.role_list = role_list;
--- commands/url.js
+++ commands/url.js
@@ -1,26 +1,52 @@
 const guld_mngr = require('./../functions/guild_manager');
 
 module.exports = async (client, message, args, portal_guilds, portal_managed_guilds_path) => {
-    const url_channel = args.join(' ').substr(0, args.join(' ').indexOf('|'));
-    const url_category = args.join(' ').substr(args.join(' ').indexOf('|') + 1);
-
-    if (url_channel !== '') {
-        guld_mngr.create_url_channel(
-            message.guild, url_channel, url_category, portal_guilds[message.guild.id].url_list);
+    if (portal_guilds[message.guild.id].url_list.find(url_list => url_list === message.channel.id) && args.length === 0) {
+        return {
+            result: true, value: '**This already is in the url list, the url channel.**'
+        };
+    } else if (portal_guilds[message.guild.id].announcement !== message.channel.id &&
+        portal_guilds[message.guild.id].spotify !== message.channel.id) {
+        if (url = message.guild.channels.cache.find(channel =>
+            portal_guilds[message.guild.id].url_list.find(url_list => url_list === channel.id))) {
+            if (url.deletable) {
+                url
+                    .delete()
+                    .catch(console.error); // edo petaei error investigate
+            }
+        } else {
+            portal_guilds[message.guild.id].url_list = [];
+        }
+    }
+    console.log('args: ', args);
+    if (args.length === 0) {
+        portal_guilds[message.guild.id].url_list.push(message.channel.id);
 
         return {
-            result: true, value: '*Url channel and category have been created*'
+            result: true, value: '**This is now the url channel.**'
         };
-    } else if (url_channel === '' && url_category !== '') {
-        guld_mngr.create_url_channel(
-            message.guild, url_category, null, portal_guilds[message.guild.id].url_list);
+    } else if (args.length > 0) {
+        const url_channel = args.join(' ').substr(0, args.join(' ').indexOf('|'));
+        const url_category = args.join(' ').substr(args.join(' ').indexOf('|') + 1);
 
-        return {
-            result: true, value: '*Url channel has been created*'
-        };
-    } else {
-        return {
-            result: false, value: '**You can run "./help url" for help.**'
-        };
+        if (url_channel !== '') {
+            guld_mngr.create_url_channel(
+                message.guild, url_channel, url_category, portal_guilds[message.guild.id].url_list);
+
+            return {
+                result: true, value: '*Url channel and category have been created*'
+            };
+        } else if (url_channel === '' && url_category !== '') {
+            guld_mngr.create_url_channel(
+                message.guild, url_category, null, portal_guilds[message.guild.id].url_list);
+
+            return {
+                result: true, value: '*Url channel has been created*'
+            };
+        } else {
+            return {
+                result: false, value: '**You can run "./help url" for help.**'
+            };
+        }
     }
 }
