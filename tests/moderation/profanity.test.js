ee39015bc7ff8967ea2913c149af494dddc35794
Ioannis Tsiakkas <keybraker@users.noreply.github.com>
2021-01-11 18:55:48 +0200

    Update profanity.test.js

--- tests/moderation/profanity.test.js
+++ tests/moderation/profanity.test.js
@@ -1,25 +1,25 @@
 const modOps = require('../../build/libraries/modOps');
 
 test('fuck must return true', () => {
-    expect(modOps.isProfane('fuck')).toBe(true);
+    expect(modOps.isProfane('fuck').length > 0 || false).toBe(true);
 });
 
 test('sample must return true', () => {
-    expect(modOps.isProfane('sample')).toBe(false);
+    expect(modOps.isProfane('sample').length > 0 || false).toBe(false);
 });
 
 test('fuckshit must return true', () => {
-    expect(modOps.isProfane('fuckshit')).toBe(true);
+    expect(modOps.isProfane('fuckshit').length > 0 || false).toBe(true);
 });
 
 test('πούτσα must return true', () => {
-    expect(modOps.isProfane('πούτσα')).toBe(true);
+    expect(modOps.isProfane('πούτσα').length > 0 || false).toBe(true);
 });
 
 test('καλημέρα must return true', () => {
-    expect(modOps.isProfane('καλημέρα')).toBe(false);
+    expect(modOps.isProfane('καλημέρα').length > 0 || false).toBe(false);
 });
 
 test('πουτσομπανάνα must return true', () => {
-    expect(modOps.isProfane('πουτσομπανάνα')).toBe(true);
+    expect(modOps.isProfane('πουτσομπανάνα').length > 0 || false).toBe(true);
 });
