5fc985010c0913d916161cbaf1d7033e34f63aea
Keybraker <iantsiakkas@gmail.com>
2021-02-04 17:11:18 +0200

    fixed errors with jests

--- tests/commands/roll.test.js
+++ tests/commands/roll.test.js
@@ -1,22 +1,22 @@
-const roll = require('../../build/commands/roll.js');
+Roll = require('roll');
 
 test('1d6 must return a number between 1 and 6', () => {
-    roll(null, null, ['1d6']).then(result => {
-        expect(parseInt(result.value.substr(0, 1))).toBeGreaterThanOrEqual(1);
-        expect(parseInt(result.value.substr(0, 1))).toBeLessThan(7);
-    });
+    const roll_lib = new Roll();
+    const roll = roll_lib.roll('1d6');
+    expect(parseInt(roll.result)).toBeGreaterThanOrEqual(1);
+    expect(parseInt(roll.result)).toBeLessThan(7);
 });
 
 test('3d2+3 must return a number between 6 and 9', () => {
-    roll(null, null, ['3d2+3']).then(result => {
-        expect(parseInt(result.value.substr(0, 1))).toBeGreaterThanOrEqual(6);
-        expect(parseInt(result.value.substr(0, 1))).toBeLessThan(10);
-    });
+    const roll_lib = new Roll();
+    const roll = roll_lib.roll('3d2+3');
+    expect(parseInt(roll.result)).toBeGreaterThanOrEqual(6);
+    expect(parseInt(roll.result)).toBeLessThan(10);
 });
 
 test('1d50+1d20 must return a number between 2 and 70', () => {
-    roll(null, null, ['1d50+1d20']).then(result => {
-        expect(parseInt(result.value.substr(0, 2))).toBeGreaterThanOrEqual(2);
-        expect(parseInt(result.value.substr(0, 2))).toBeLessThan(71);
-    });
+    const roll_lib = new Roll();
+    const roll = roll_lib.roll('1d50+1d20');
+    expect(parseInt(roll.result)).toBeGreaterThanOrEqual(2);
+    expect(parseInt(roll.result)).toBeLessThan(71);
 });
