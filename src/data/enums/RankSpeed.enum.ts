b0417466d21b4b1095e9128937e65fe0a2ac8008
Ioannis Tsiakkas <iantsiakkas@gmail.com>
2021-03-12 16:40:52 -0800

    Update  ranking  system with new enums and db

--- src/data/enums/RankSpeed.enum.ts
+++ src/data/enums/RankSpeed.enum.ts
@@ -1,13 +1,20 @@
 export enum RankSpeedEnum {
 	none,
-    slow,
+	slow,
 	default,
 	fast
 };
 
 export const RankSpeedList = [
 	'none',
-    'slow',
+	'slow',
 	'default',
 	'fast'
+];
+
+export const RankSpeedValueList = [
+	0.00,
+	0.05,
+	0.10,
+	0.15
 ];
