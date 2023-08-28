c0d164c1bb38cb3bb35a1408d94aecba980a6082
ryzenbeast\Ioannis Tsiakkas <iantsiakkas@gmail.com>
2023-08-28 20:31:57 +0300

    Update kick in accordance to ban

--- src/commands/auth/kick.ts
+++ src/commands/auth/kick.ts
@@ -1,4 +1,7 @@
 import { SlashCommandBuilder } from '@discordjs/builders';
+import { BanOptions, ChatInputCommandInteraction, GuildMember } from 'discord.js';
+import { askForApproval, isMod, messageHelp } from '../../libraries/help.library';
+import { ban } from '../../libraries/user.library';
 import { Command } from '../../types/Command';
 import { ReturnPromise, ScopeLimit } from '../../types/classes/PTypes.interface';
 
@@ -13,111 +16,102 @@ export = {
   scopeLimit: ScopeLimit.MEMBER,
   slashCommand: new SlashCommandBuilder()
     .setName(COMMAND_NAME)
-    .setDescription(DESCRIPTION),
-  async execute(/* interaction: ChatInputCommandInteraction */): Promise<ReturnPromise> {
+    .setDescription(DESCRIPTION)
+    .addUserOption(option =>
+      option
+        .setName('user_to_ban')
+        .setDescription('user to ban')
+        .setRequired(true))
+    .addNumberOption(option =>
+      option
+        .setName('ban_days')
+        .setDescription('days to ban user for')
+        .setRequired(true))
+    .addStringOption(option =>
+      option
+        .setName('ban_reason')
+        .setDescription('ban reason')
+        .setRequired(false))
+    .setDMPermission(false),
+  async execute(interaction: ChatInputCommandInteraction): Promise<ReturnPromise> {
+    const memberToBan = interaction.options.getMember('user_to_ban') as GuildMember;
+    const banDays = interaction.options.getNumber('ban_days');
+    const banReason = interaction.options.getString('ban_reason');
+
+    if (!memberToBan) {
+      return {
+        result: false,
+        value: messageHelp('commands', 'corona', 'user must be provided'),
+      };
+    }
+
+    if (!banDays) {
+      return {
+        result: false,
+        value: messageHelp('commands', 'corona', 'days to ban must be provided'),
+      };
+    }
+
+    if (!interaction.member) {
+      return {
+        result: false,
+        value: 'message author could not be fetched',
+      };
+    }
+
+    if (!isMod((interaction.member as GuildMember))) {
+      return {
+        result: false,
+        value: 'you must be a Portal moderator to ban users',
+      };
+    }
+
+    if (!interaction.guild) {
+      return {
+        result: false,
+        value: 'user guild could not be fetched',
+      };
+    }
+
+    const deleteMessageDays = banDays ?? 1;
+    const reason = banReason ?? 'banned by admin';
+
+    const response = await askForApproval(interaction, memberToBan.displayName);
+
+    try {
+      const confirmation = await response.awaitMessageComponent({
+        filter: buttonInteraction => buttonInteraction.user.id === interaction.user.id,
+        time: 10_000
+      });
+
+      if (confirmation.customId === 'confirm') {
+        const banOptions: BanOptions = {
+          deleteMessageDays,
+          reason,
+        };
+
+        const banResponse = await ban(memberToBan, banOptions);
+        await confirmation.update({
+          content: banResponse
+            ? `${memberToBan} has been banned by ${interaction.user} ` +
+          `for ${banDays} ${deleteMessageDays > 1 ? 'days' : 'day'}, because: *${reason}*`
+            : `${memberToBan} is not bannable`
+        });
+      } else if (confirmation.customId === 'cancel') {
+        await confirmation.update({
+          content: 'Ban cancelled'
+        });
+      }
+    } catch (e) {
+      await interaction.editReply({ content: 'Confirmation not received within 1 minute, cancelling', components: [] });
+    }
+
     return {
-      result: false,
-      value: 'not yet implemented',
-    };
-
-    // const member = interaction.member as GuildMember;
-    // if (!member) {
-    //   return {
-    //     result: false,
-    //     value: 'message author could not be fetched',
-    //   };
-    // }
-
-    // if (!isMod(member)) {
-    //   return {
-    //     result: false,
-    //     value: `you must be a Portal moderator to kick users`,
-    //   };
-    // }
-
-    // if (!interaction.guild) {
-    //   return {
-    //     result: false,
-    //     value: `user guild could not be fetched`,
-    //   };
-    // }
-
-    // let kickReason = args
-    //   .join(' ')
-    //   .substring(args.join(' ').indexOf('|') + 1)
-    //   .replace(/\s/g, ' ')
-    //   .trim();
-
-    // if (kickReason === '') {
-    //   kickReason = 'kicked by admin';
-    // }
-
-    // if (interaction.mentions && interaction.mentions.members) {
-    //   if (interaction.mentions.members.size === 0) {
-    //     return {
-    //       result: false,
-    //       value: messageHelp('commands', 'kick', `you must tag a member`),
-    //     };
-    //   }
-
-    //   const memberToKick = interaction.mentions.members.first() || interaction.guild.members.cache.get(args[0]);
-
-    //   if (memberToKick) {
-    //     if (member === memberToKick) {
-    //       return {
-    //         result: false,
-    //         value: messageHelp('commands', 'kick', `you can't kick on yourself`),
-    //       };
-    //     }
-
-    //     askForApproval(
-    //       interaction,
-    //       member,
-    //       `*${member}, are you sure you want to kick ` + `user ${memberToKick}*, do you **(yes / no)** ?`
-    //     )
-    //       .then((result) => {
-    //         if (result) {
-    //           kick(memberToKick, kickReason)
-    //             .then((r) => {
-    //               return {
-    //                 result: r,
-    //                 value: r
-    //                   ? `${memberToKick} has been kicked by ${interaction.author} ` + `because: *${kickReason}*`
-    //                   : `${memberToKick} is not kickable`,
-    //               };
-    //             })
-    //             .catch((e) => {
-    //               return {
-    //                 result: false,
-    //                 value:
-    //                   `failed to kick member ${memberToKick}, ` +
-    //                   `Portal's role must be higher than member you want to kick: ${e}`,
-    //               };
-    //             });
-    //         } else {
-    //           return {
-    //             result: false,
-    //             value: `user ${memberToKick} will not be kicked`,
-    //           };
-    //         }
-    //       })
-    //       .catch((e) => {
-    //         return {
-    //           result: false,
-    //           value: `failed to kick: ${e}`,
-    //         };
-    //       });
-    //   } else {
-    //     return {
-    //       result: false,
-    //       value: `could not find member`,
-    //     };
-    //   }
-    // } else {
-    //   return {
-    //     result: false,
-    //     value: `no user mentioned to kick`,
-    //   };
-    // }
+      result: true,
+      value: '',
+    }
   },
 } as Command;
+
+
+
